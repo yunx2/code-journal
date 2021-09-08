6d61eafb21cca1b14840020e916c211827954a5b
Yunyun Liu <celeph4is@gmail.com>
2021-09-07 23:41:16 -0700

    Define a function that takes a single journal entry object and returns a DOM tree that matches one of the example entries in the HTML.issue#2

--- js/main.js
+++ js/main.js
@@ -2,17 +2,13 @@
 /* exported data */
 
 const $entryForm = document.getElementById('entry-form');
-
 const $photoUrlInput = $entryForm.elements[1];
 const $photoPreview = document.getElementById('photo-preview');
-
 
 function handleUrlInput(e) {
   $photoPreview.setAttribute('src', e.target.value);
 }
-
 $photoUrlInput.addEventListener('input', e => handleUrlInput(e));
-
 
 function handleSubmit(e) {
   e.preventDefault();
@@ -31,11 +27,55 @@ function handleSubmit(e) {
   const placeholderUrl = './images/placeholder-image-square.jpg';
   $photoPreview.setAttribute('src', placeholderUrl);
 }
-
 $entryForm.addEventListener('submit', e => handleSubmit(e));
 
 function handleUnload() {
   localStorage.setItem('prevEntriesJSON', JSON.stringify(data.entries));
 }
-
 window.addEventListener('beforeunload', handleUnload);
+
+const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosq.';
+
+const dummyEntry = {
+  entryId: 13,
+  journalEntry: lorem,
+  title: 'Scheme',
+  photoUrl: 'images/dummy-images/scheme.jpg'
+};
+
+function createEntryElement(entry) {
+  const { entryId, journalEntry, photoUrl, title } = entry;
+  // first create container elements
+  const $entriesListItem = document.createElement('li'); // outermost element
+
+  const $article = document.createElement('article');
+  $article.classList.add('row');
+  $article.setAttribute('data-entryId', entryId);
+  // children of $article
+  const $colHalfImg = document.createElement('div');
+  $colHalfImg.classList.add('column-half');
+  const $colHalfEntry = document.createElement('div');
+  $colHalfEntry.classList.add('column-half');
+  // elements that use data from entry object
+  const $photo = document.createElement('img');
+  $photo.classList.add('entry-img');
+  $photo.setAttribute('src', photoUrl);
+  $colHalfImg.append($photo);
+
+  const $title = document.createElement('h3');
+  $title.classList.add('entry-title');
+  $title.textContent = title;
+  const $entryPara = document.createElement('p');
+  $entryPara.textContent = journalEntry;
+  $colHalfEntry.append($title, $entryPara);
+
+  $article.append($colHalfImg, $colHalfEntry);
+
+  $entriesListItem.appendChild($article);
+  // console.log('$entriesListItem:', $entriesListItem);
+  // console.log('entryId:', entryId);
+  // console.log('title:', title);
+  // console.log('entryId:', entryId);
+}
+
+createEntryElement(dummyEntry);
