36925390f26c43cbdac2698d554dc80281f8a456
Yunyun Liu <celeph4is@gmail.com>
2021-09-09 10:35:11 -0700

    move code related to storing/accessing local storage to data.js file

--- js/data.js
+++ js/data.js
@@ -4,6 +4,10 @@
 // because all the data in it needs to persist
 // entry being edited goes at data.entry
 // data.nextEntryId gets incremented
+
+// this file gets loaded first and main.js second. so functions in main.js can use the functions
+// and variables defined in this file
+
 var data = {
   view: 'entry-form',
   entries: [],
@@ -11,25 +15,93 @@ var data = {
   nextEntryId: 1
 };
 
-// const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosq.';
+let entries = [];
+let idCount = 1;
+let view = 'entry-form';
 
-// const scheme = {
-//   entryId: 13,
-//   journalEntry: lorem,
-//   title: 'Scheme',
-//   photoUrl: 'images/dummy-images/scheme.jpg'
-// };
+const $entriesList = document.getElementById('entries-ul');
 
-// const javascript = {
-//   entryId: 8,
-//   journalEntry: lorem,
-//   title: 'Javascript',
-//   photoUrl: './images/dummy-images/javascript.png'
-// };
+function swapView() {
+  const viewNodeList = document.querySelectorAll('.view');
+  viewNodeList.forEach(node => {
+    const data = node.getAttribute('data-view');
+    if (data === view) {
+      node.classList.remove('hidden');
+      node.scrollIntoView();
+    } else {
+      node.classList.add('hidden');
+    }
+  });
+}
 
-// const node = {
-//   entryId: 4,
-//   journalEntry: lorem,
-//   title: 'Node',
-//   photoUrl: '/images/dummy-images/node.png'
-// };
+// handle load and unload
+function handleUnload() {
+  data.entries = entries;
+  data.nextEntryId = idCount;
+  data.view = view;
+  const dataJSON = JSON.stringify(data);
+  localStorage.setItem('dataJSON', dataJSON);
+}
+
+function createEntryElement(entry) {
+  const { entryId, journalEntry, photoUrl, title } = entry;
+  // first create container elements
+  const $entriesListItem = document.createElement('li'); // outermost element
+  const $article = document.createElement('article');
+  $article.classList.add('row');
+  $article.setAttribute('data-entryId', entryId);
+  // children of $article
+  const $colHalfImg = document.createElement('div');
+  $colHalfImg.classList.add('column-half');
+  const $colHalfEntry = document.createElement('div');
+  $colHalfEntry.classList.add('column-half');
+  // elements that use data from entry object
+  const $photo = document.createElement('img');
+  $photo.classList.add('entry-img');
+  $photo.setAttribute('src', photoUrl);
+  $colHalfImg.append($photo);
+  const $title = document.createElement('h3');
+  $title.classList.add('entry-title');
+  $title.textContent = title;
+  const $entryPara = document.createElement('p');
+  $entryPara.textContent = journalEntry;
+  $colHalfEntry.append($title, $entryPara);
+  $article.append($colHalfImg, $colHalfEntry);
+  // append article to outermost element
+  $entriesListItem.appendChild($article);
+  // return outermost element
+  return $entriesListItem;
+}
+
+function handlePageLoad() {
+  // access localstorage at key 'dataJSON' and parse value back into JS
+  const storedData = JSON.parse(localStorage.getItem('dataJSON'));
+  // intialized entries, idCount, and view variables with data from stored object
+  entries = storedData.entries;
+  idCount = storedData.nextEntryId;
+  view = storedData.view;
+  swapView();
+  // check that the entries array exists and has at least on eentry in it
+  if (entries && (entries.length > 0)) {
+    // hide 'no entries' message
+    const $message = document.getElementById('no-entries-recorded');
+    $message.classList.add('hidden');
+    // entries is already in reverse chronologiccal order, so just append
+    // in the same order as array
+    entries.forEach(current => {
+      const $current = createEntryElement(current);
+      $entriesList.append($current);
+    });
+  }
+}
+// attach event listeners
+window.addEventListener('beforeunload', handleUnload);
+document.addEventListener('DOMContentLoaded', handlePageLoad);
+
+function clearData() {
+  localStorage.clear();
+  entries = [];
+  idCount = 1;
+  view = 'entry-form';
+  window.location.reload();
+}
--- js/main.js
+++ js/main.js
@@ -1,49 +1,9 @@
 /* global data */
 /* exported data */
-let entries = [];
-let idCount = 1;
-let view = 'entry-form';
+
 const $entryForm = document.getElementById('entry-form');
 const $photoUrlInput = $entryForm.elements[1];
 const $photoPreview = document.getElementById('photo-preview');
-
-function handleUnload() {
-  data.entries = entries;
-  data.nextEntryId = idCount;
-  data.view = view;
-  const dataJSON = JSON.stringify(data);
-  localStorage.setItem('dataJSON', dataJSON);
-}
-
-function createEntryElement(entry) {
-  const { entryId, journalEntry, photoUrl, title } = entry;
-  // first create container elements
-  const $entriesListItem = document.createElement('li'); // outermost element
-  const $article = document.createElement('article');
-  $article.classList.add('row');
-  $article.setAttribute('data-entryId', entryId);
-  // children of $article
-  const $colHalfImg = document.createElement('div');
-  $colHalfImg.classList.add('column-half');
-  const $colHalfEntry = document.createElement('div');
-  $colHalfEntry.classList.add('column-half');
-  // elements that use data from entry object
-  const $photo = document.createElement('img');
-  $photo.classList.add('entry-img');
-  $photo.setAttribute('src', photoUrl);
-  $colHalfImg.append($photo);
-  const $title = document.createElement('h3');
-  $title.classList.add('entry-title');
-  $title.textContent = title;
-  const $entryPara = document.createElement('p');
-  $entryPara.textContent = journalEntry;
-  $colHalfEntry.append($title, $entryPara);
-  $article.append($colHalfImg, $colHalfEntry);
-  // append article to outermost element
-  $entriesListItem.appendChild($article);
-  // return outermost element
-  return $entriesListItem;
-}
 
 function createAndAdd(entry) {
   // create DOM element from entry object
@@ -52,46 +12,6 @@ function createAndAdd(entry) {
   document.getElementById('no-entries-recorded').classList.add('hidden');
   const $firstListItem = document.querySelector('li:first-child');
   $firstListItem.prepend($el);
-}
-
-const viewNodeList = document.querySelectorAll('.view');
-
-function swapView() {
-  viewNodeList.forEach(node => {
-    const data = node.getAttribute('data-view');
-    if (data === view) {
-      node.classList.remove('hidden');
-      node.scrollIntoView();
-    } else {
-      node.classList.add('hidden');
-    }
-  });
-}
-
-const $entriesList = document.getElementById('entries-ul');
-
-function handlePageLoad() {
-  // access localstorage at key 'dataJSON' and parse value back into JS
-  const storedData = JSON.parse(localStorage.getItem('dataJSON'));
-  // intialized entries, idCount, and view variables with data from stored object
-  entries = storedData.entries;
-  idCount = storedData.nextEntryId;
-  view = storedData.view;
-  swapView();
-  // check that the entries array exists and has at least on eentry in it
-  if (entries && (entries.length > 0)) {
-    // hide 'no entries' message
-    const $message = document.getElementById('no-entries-recorded');
-    $message.classList.add('hidden');
-    // iterate through storedEntries, create a <li> element from each entry object;
-    // attach created elements to DOM
-    // entries is already in reverse chronologiccal order, so just append
-    // in the same order as array
-    entries.forEach(current => {
-      const $current = createEntryElement(current);
-      $entriesList.append($current);
-    });
-  }
 }
 
 // 'DOMContentLoaded' event fires after HTML document has been loaded; doesn't wait for stylesheets/images/etc
@@ -126,13 +46,7 @@ function handleSubmit(e) {
 $entryForm.addEventListener('submit', e => handleSubmit(e));
 $photoUrlInput.addEventListener('input', e => handleUrlInput(e));
 
-window.addEventListener('beforeunload', handleUnload);
-document.addEventListener('DOMContentLoaded', handlePageLoad);
-
 // click handlers for view-swapping
-// handlers change the value of view variable and add 'hidden' class to all other views
-// all 'active' to selected view
-
 const $entriesButton = document.getElementById('btn-entries');
 $entriesButton.addEventListener('click', () => {
   view = 'entries';
@@ -144,11 +58,3 @@ $newButton.addEventListener('click', () => {
   view = 'entry-form';
   swapView();
 });
-
-// function clearData() {
-//   localStorage.clear();
-//   entries = [];
-//   idCount = 1;
-//   view = 'entry-form';
-//   window.location.reload();
-// }
